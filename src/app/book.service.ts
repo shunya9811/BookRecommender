190a2edbf55fa9e6147881d54f4255e70a1c6ff2
shunya9811 <11wash27@gmail.com>
2023-01-10 19:25:21 +0900

    api url change

--- src/app/book.service.ts
+++ src/app/book.service.ts
@@ -12,7 +12,7 @@ import { catchError, map } from 'rxjs/operators';
 })
 export class BookService {
 
-  private bookUrl: string = "http://openlibrary.org/search.json?q=";
+  private bookUrl: string = "https://openlibrary.org/search.json?q=";
   
   book$: Book | any = null;
   bookList$: Book[] = [];
